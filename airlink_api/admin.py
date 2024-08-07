41266330e0af1f7420abbacee8df38523ce0054e
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-08-07 13:38:59 +0300

    add all models into admin panel

--- airlink_api/admin.py
+++ airlink_api/admin.py
@@ -1,1 +1,20 @@
-# Register your models here.
+from django.contrib import admin
+
+from airlink_api.models import (
+    AirplaneType,
+    Airplane,
+    Airport,
+    Crew,
+    Route,
+    Flight,
+    Order,
+    Ticket
+)
+admin.site.register(AirplaneType)
+admin.site.register(Airplane)
+admin.site.register(Airport)
+admin.site.register(Crew)
+admin.site.register(Route)
+admin.site.register(Flight)
+admin.site.register(Order)
+admin.site.register(Ticket)
